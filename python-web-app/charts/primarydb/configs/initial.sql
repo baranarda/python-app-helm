6341899238234a8b947ff5467519ecf66c7d2ce7
baranarda <100856570+baranarda@users.noreply.github.com>
2022-03-03 17:16:09 +0200

    Delete python-web-app directory

--- python-web-app/charts/primarydb/configs/initial.sql
+++
@@ -1,9 +0,0 @@
-SET password_encryption = 'scram-sha-256';
-CREATE ROLE repuser WITH REPLICATION PASSWORD 'changeme' LOGIN;
-SELECT * FROM pg_create_physical_replication_slot('replica_1_slot');
-CREATE TABLE blacklist (
-    id serial PRIMARY KEY,
-	path VARCHAR ( 50 ),
-	ipaddress VARCHAR ( 50 ),
-    timestamp TIMESTAMP 
-);
